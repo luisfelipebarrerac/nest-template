ebb1e2d4e0dd102642645186cbf733a4f4bfca16
luisfelipebarrerac <51801316+luisfelipebarrerac@users.noreply.github.com>
2022-01-03 01:02:47 -0500

    auth/signUp

---
+++ src/auth/dto/sign-up.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+}
